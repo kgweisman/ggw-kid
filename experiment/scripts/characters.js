69a15ccdb73ae693db80728776b99b4172fc4bb7
kgweisman <kweisman@stanford.edu>
2015-05-01 16:14:02 -0700

    swap in new characters

--- experiment/scripts/characters.js
+++ experiment/scripts/characters.js
@@ -4,7 +4,7 @@ function addCharacter(charName, charTitle, charDescrip) {
 	function Character(charName, charTitle, charDescrip) {
 		this.charName = charName;
 		this.charTitle = charTitle;
-		this.imageSource = "images_characters/"+charName+".png";
+		this.imageSource = "images_characters/"+charName+".jpg";
 		this.charDescrip = charDescrip;
 	};
 	newCharacter = new Character(charName, charTitle, charDescrip);
@@ -14,34 +14,20 @@ function addCharacter(charName, charTitle, charDescrip) {
 characters = {};
 
 // closer to original...
-addCharacter("charlie_dog", "Charlie, family dog", "Charlie is a 3-year-old Springer spaniel and a beloved member of the Graham family.");
-addCharacter("delores_gleitman_deceased", "Delores Gleitman, recently deceased", "Delores Gleitman recently passed away at the age of 65. As you complete the survey, please draw upon your own personal beliefs about people who have passed away.");
-addCharacter("fetus", "7-week human fetus", "At 7 weeks, a human fetus is almost half an inch long - roughly the size of a raspberry.");
-addCharacter("gerald_schiff_pvs", "Gerald Schiff, in a persistent vegetative state (similar to a coma)", "Gerald Schiff has been in a persistent vegetative state (PVS) for the past six months. Although he has severe brain damage - Gerald does not appear to communicate with others or make purposeful movements - his basic bodily functions (such as breathing, sleeping, and circulation) are preserved.");
-addCharacter("god", "God", "Many people believe that God is the creator of the universe and the ultimate source of knowledge, power, and love. However, please draw upon your own personal beliefs about God.");
-addCharacter("green_frog", "Green frog", "The Green Frog can be found throughout eastern North America. This classic 'pond frog' is medium-sized and green or bronze in color. Daily life includes seeking out permanent ponds or slow streams with plenty of vegetation.");
-addCharacter("kismet_robot", "Kismet, sociable robot", "Kismet is part of a new class of 'sociable' robots that can engage people in natural interaction. To do this, Kismet perceives a variety of natural social signals from sound and sight, and delivers his own signals back to the human partner through gaze direction, facial expression, body posture, and vocal babbles.");
-addCharacter("nicholas_gannon_baby", "Nicholas Gannon, 5-month-old", "Nicholas is a five-month-old baby.");
-addCharacter("samantha_hill_girl", "Samantha Hill, 5-year-old", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
-addCharacter("sharon_harvey_woman", "Sharon Harvey, advertising executive", "Sharon Harvey, 38, works at an advertising agency in Chicago.");
-addCharacter("toby_chimp", "Wild chimpanzee", "Toby is a two-year-old wild chimpanzee living at an outdoor laboratory in Uganda.");
-addCharacter("todd_billingsley_man", "Todd Billingsley, accountant", "Todd Billingsley is a thirty-year-old accountant who lives in New York City.");
-addCharacter("you", "You", "When you see the mirror, please consider how you, yourself, would compare with the other choice presented.");
-
-// more minimal...
-// addCharacter("charlie_dog", "Family dog", "Charlie is a 3-year-old Springer spaniel and a beloved member of the Graham family.");
-// addCharacter("delores_gleitman_deceased", "Recently deceased woman", "Delores Gleitman recently passed away at the age of 65. As you complete the survey, please draw upon your own personal beliefs about people who have passed away.");
-// addCharacter("fetus", "7-week human fetus", "At 7 weeks, a human fetus is almost half an inch long - roughly the size of a raspberry.");
-// addCharacter("gerald_schiff_pvs", "Man in a persistent vegetative state (similar to a coma)", "Gerald Schiff has been in a persistent vegetative state (PVS) for the past six months. Although he has severe brain damage - Gerald does not appear to communicate with others or make purposeful movements - his basic bodily functions (such as breathing, sleeping, and circulation) are preserved.");
-// addCharacter("god", "God", "Many people believe that God is the creator of the universe and the ultimate source of knowledge, power, and love. However, please draw upon your own personal beliefs about God.");
-// addCharacter("green_frog", "Frog", "The Green Frog can be found throughout eastern North America. This classic 'pond frog' is medium-sized and green or bronze in color. Daily life includes seeking out permanent ponds or slow streams with plenty of vegetation.");
-// addCharacter("kismet_robot", "Robot", "Kismet is part of a new class of 'sociable' robots that can engage people in natural interaction. To do this, Kismet perceives a variety of natural social signals from sound and sight, and delivers his own signals back to the human partner through gaze direction, facial expression, body posture, and vocal babbles.");
-// addCharacter("nicholas_gannon_baby", "5-month-old baby", "Nicholas is a five-month-old baby.");
-// addCharacter("samantha_hill_girl", "5-year-old child", "Samantha is a five-year-old girl who lives with her parents and older sister Jennifer.");
-// addCharacter("sharon_harvey_woman", "Adult woman", "Sharon Harvey, 38, works at an advertising agency in Chicago.");
-// addCharacter("toby_chimp", "Wild chimpanzee", "Toby is a two-year-old wild chimpanzee living at an outdoor laboratory in Uganda.");
-// addCharacter("todd_billingsley_man", "Adult man", "Todd Billingsley is a thirty-year-old accountant who lives in New York City.");
-// addCharacter("you", "You", "When you see the mirror, please consider how you, yourself, would compare with the other choice presented.");
+addCharacter("baby", "baby", "...");
+addCharacter("bear", "bear", "...");
+addCharacter("bug", "bug", "...");
+addCharacter("car", "car", "...");
+addCharacter("computer", "computer", "...");
+addCharacter("dog", "dog", "...");
+addCharacter("grownup", "grown-up", "...");
+addCharacter("kid", "kid", "...");
+addCharacter("moon", "moon", "...");
+addCharacter("mountain", "mountain", "...");
+addCharacter("robot", "robot", "...");
+addCharacter("stapler", "stapler", "...");
+addCharacter("teddybear", "teddy bear", "...");
+addCharacter("tree", "tree", "...");
 
 // create the list of all possible pairs (78)
 
