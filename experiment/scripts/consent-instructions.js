c3f207489632656f5f3467d98e79b9f550573ab4
kgweisman <kweisman@stanford.edu>
2015-05-06 23:49:48 -0700

    manually enter (and require) subid

--- experiment/scripts/consent-instructions.js
+++ experiment/scripts/consent-instructions.js
@@ -4,11 +4,28 @@
 // 	showSlide('instructions');
 // })
 
+// $('.slide#start button').click(function() {
+// 	if(turk.previewMode === false) {
+// 		charactersSlide.makeOrder();
+// 		charactersSlide.next();
+// 		window.scrollTo(0, 0);
+// 		showSlide('characters');
+// 	}
+// })
+
 $('.slide#start button').click(function() {
-	if(turk.previewMode === false) {
+
+	// if no subid, prevent progress
+	if($('input#subid').val() === "") {
+		window.alert("Please enter a subid.");
+
+	} else {
+		// record subid in experiment object
+		experiment.newData.subid = $('input#subid').val();
 		charactersSlide.makeOrder();
 		charactersSlide.next();
 		window.scrollTo(0, 0);
 		showSlide('characters');
 	}
 })
+
